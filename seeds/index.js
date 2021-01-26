6abcbb51df5e67d96b842cbd14b45261f453aa68
Rohit Bhati <rohitbhati8833@gmail.com>
2021-01-26 22:40:59 +0530

    Added CRUD for Campgrounds
    Creating, reading, updating and deleting campground from the mongoDB using mongoose with express and nodeJS

---
+++ seeds/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const {places, descriptors} = require('./seedHelpers');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+const sample = data => data[Math.floor(Math.random() * data.length)];
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location : `${cities[random1000].city}, ${cities[random1000].state}`,
+            title : `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+seedDB().then(() => {
+    mongoose.connection.close();
+})
