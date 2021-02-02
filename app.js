e879b9b8d015bfc683b3a0ad49b8f6eeace533e7
Rohit Bhati <rohitbhati8833@gmail.com>
2021-02-03 00:48:39 +0530

    Error Handling was implemented. Form Validations were done on both client and server side.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
+const {campgroundSchema} = require('./schemas');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -19,50 +22,76 @@ db.once("open", () => {
 
 const app = express();
 
-app.engine('ejs',ejsMate);
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'views'));
-app.use(express.urlencoded({extended : true}));
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req,res,next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg,400);
+    }
+    else{
+        next();
+    }
+    // console.log(result);
 
-app.get('/',(req,res) => {
+}
+
+app.get('/', (req, res) => {
     // res.send('Hello from Yelp Camp');
     res.render('home');
 })
-app.get('/campgrounds',async (req,res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-})
-app.get('/campgrounds/new',(req,res) => {
+    res.render('campgrounds/index', { campgrounds });
+}))
+app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
-app.post('/campgrounds', async (req,res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     // console.log(req.body);
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
-app.get('/campgrounds/:id',async (req,res) => {
+}))
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground});
-})
-app.get('/campgrounds/:id/edit', async (req,res) => {
+    res.render('campgrounds/show', { campground });
+}))
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground});
-})
-app.put('/campgrounds/:id', async (req,res) => {
+    res.render('campgrounds/edit', { campground });
+}))
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     // res.send('It Worked!!!');
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-})
-app.delete('/campgrounds/:id',async (req,res) => {
-    const {id} = req.params;
+}))
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
 })
 
-app.listen(3000,() => {
+app.use((err, req, res, next) => {
+    // const {statusCode = 500, message = 'Something Went Wrong'} = err;
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!😐';
+    res.status(statusCode).render('error', { err });
+    // res.send('Are bhai bhai bhai bhai!!');
+})
+
+app.listen(3000, () => {
     console.log('Serving from Port 3000');
 });
