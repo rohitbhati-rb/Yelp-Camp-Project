bc714242ecbd2fa216524d254ac7fe735bec9f87
Rohit Bhati <rohitbhati8833@gmail.com>
2021-02-14 10:31:38 +0530

    Authentication for Login was implemented.

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
@@ -19,5 +20,12 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('register');
     }
 }))
+router.get('/login', (req,res) => {
+    res.render('users/login');
+})
+router.post('/login', passport.authenticate('local', {failureFlash : true, failureRedirect : '/login'}) , (req,res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
