af81d5dee76cf12ed7ae898da45226f8e47ed710
Rohit Bhati <rohitbhati8833@gmail.com>
2021-02-19 20:07:52 +0530

    Image deletion from cloud and mongo was implemented

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,6 @@
 const Campground = require('../models/campground');
+const {cloudinary} = require('../cloudinary');
+
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
@@ -10,7 +12,7 @@ module.exports.createCampground = async (req, res, next) => {
     // console.log(req.body);
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({url : f.path, filename : f.filename}));
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
     // console.log(campground);
@@ -44,10 +46,18 @@ module.exports.updateCampground = async (req, res) => {
     // res.send('It Worked!!!');
     if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const { id } = req.params;
+    // console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    const imgs = req.files.map(f => ({url : f.path, filename : f.filename}));
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save();
+    if (req.body.deleteImages) {
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } });
+        // console.log(campground);
+    }
     req.flash('success', 'Successfully updated the campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }
