4300a3bf686653e0a49c3e3ad85f5bb05b088014
Rohit Bhati <rohitbhati8833@gmail.com>
2021-03-02 21:51:27 +0530

    navbar and image carousel bug fixed

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-// const dbUrl = process.env.DB_URL;
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// const dbUrl = process.env.DB_URL; process.env.DB_URL ||
+const dbUrl =  'mongodb://localhost:27017/yelp-camp';
 // 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
